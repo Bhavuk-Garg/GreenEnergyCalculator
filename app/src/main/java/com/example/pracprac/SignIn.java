e1669e466550474bfc91ca93146a23b9c69d9201
bhargav462 <bhargavnakkina462@gmail.com>
2019-03-02 15:18:08 +0530

    changes in sign in sign up

--- app/src/main/java/com/example/pracprac/SignIn.java
+++ app/src/main/java/com/example/pracprac/SignIn.java
@@ -1,5 +1,6 @@
 package com.example.pracprac;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
 import android.support.annotation.NonNull;
@@ -8,8 +9,10 @@ import android.os.Bundle;
 import android.util.Log;
 import android.util.Patterns;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
@@ -171,4 +174,26 @@ public class SignIn extends AppCompatActivity  {
                 startActivity(new Intent(SignIn.this,Admin.class));
         }
     }
+
+//@Override
+//public boolean onTouchEvent(MotionEvent event) {
+//    InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+//    imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+//    return true;
+//}
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        View view = getCurrentFocus();
+        if (view != null && (ev.getAction() == MotionEvent.ACTION_UP || ev.getAction() == MotionEvent.ACTION_MOVE) && view instanceof EditText && !view.getClass().getName().startsWith("android.webkit.")) {
+            int scrcoords[] = new int[2];
+            view.getLocationOnScreen(scrcoords);
+            float x = ev.getRawX() + view.getLeft() - scrcoords[0];
+            float y = ev.getRawY() + view.getTop() - scrcoords[1];
+            if (x < view.getLeft() || x > view.getRight() || y < view.getTop() || y > view.getBottom())
+                ((InputMethodManager)this.getSystemService(Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow((this.getWindow().getDecorView().getApplicationWindowToken()), 0);
+        }
+        return super.dispatchTouchEvent(ev);
+    }
+
 }
