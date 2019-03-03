5a43be4372d91c5ad44268e11a13f6d89d77127d
Bhavuk Garg <bhavukgarg5678@gmail.com>
2019-03-03 06:36:01 +0530

    Updated SingIn and SignUp also set colours

--- app/src/main/java/com/example/pracprac/solar_daily_adapter.java
+++ app/src/main/java/com/example/pracprac/solar_daily_adapter.java
@@ -4,23 +4,28 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import java.util.List;
 
 public class solar_daily_adapter extends ArrayAdapter<data_class> {
+
+    Animation animation;
+
     public solar_daily_adapter(Context context, List<data_class> energydata){
         super(context,0,energydata);
-
+        animation= AnimationUtils.loadAnimation(context,R.anim.righttoleft);
     }
-
 
     @Override
     public View getView(int position, View convertview, ViewGroup parent){
        View listitemview=convertview;
        if (listitemview==null){
            listitemview= LayoutInflater.from(getContext()).inflate(R.layout.listviewsolardaily,parent,false);
+           listitemview.startAnimation(animation);
         }
 
 
